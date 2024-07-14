501b3d7373b19414a45902a5cb07f08d5f7f26d8
postech-ml <166416040+postech-ml@users.noreply.github.com>
2024-07-14 21:52:17 +0000

    alt app

--- app.py
+++ app.py
@@ -21,22 +21,20 @@ def chamar_extracao():
         print(f"Erro na extração: {e}")
         return jsonify({"message": f"Erro na extração: {e}"}), 500
 
-# @app.route('/ingestao')
-# def chamar_ingestao():
-#     try:
-#         csv_file = extrair_dados()
-#         if csv_file:
-#             Ingestao.processar_dados(csv_file)
-#             print("Ingestão concluída com sucesso.")
-#             return jsonify({"message": "Ingestão concluída com sucesso."})
-#         else:
-#             print("Ingestão não realizada devido a erro na extração.")
-#             return jsonify({"message": "Ingestão não realizada devido a erro na extração."}), 500
-#     except Exception as e:
-#         print(f"Erro na ingestão: {e}")
-#         return jsonify({"message": f"Erro na ingestão: {e}"}), 500
+@app.route('/ingestao')
+def chamar_ingestao():
+    try:
+        csv_file = extrair_dados()
+        if csv_file:
+            Ingestao.processar_dados(csv_file)
+            print("Ingestão concluída com sucesso.")
+            return jsonify({"message": "Ingestão concluída com sucesso."})
+        else:
+            print("Ingestão não realizada devido a erro na extração.")
+            return jsonify({"message": "Ingestão não realizada devido a erro na extração."}), 500
+    except Exception as e:
+        print(f"Erro na ingestão: {e}")
+        return jsonify({"message": f"Erro na ingestão: {e}"}), 500
 
 if __name__ == "__main__":
-    csv_file = chamar_extracao()
-    # chamar_ingestao(csv_file)
     app.run(debug=True)
