922d591fc209af6d3eb89478af017ae5682c5e8a
postech-ml <166416040+postech-ml@users.noreply.github.com>
2024-07-14 21:54:58 +0000

    alt app

--- app.py
+++ app.py
@@ -1,40 +1,29 @@
 from extracao import extrair_dados
 from ingestao import Ingestao
-from flask import Flask, jsonify
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "Aplicação de Extração e Ingestão de Dados"
-
-@app.route('/extrair')
 def chamar_extracao():
     try:
         csv_file = extrair_dados()
         if csv_file:
             print("Extração concluída com sucesso.")
-            return jsonify({"message": "Extração concluída com sucesso.", "file": csv_file})
+            return csv_file
         else:
-            return jsonify({"message": "Erro na extração."}), 500
+            print("Erro na extração.")
+            return None
     except Exception as e:
         print(f"Erro na extração: {e}")
-        return jsonify({"message": f"Erro na extração: {e}"}), 500
+        return None
 
-@app.route('/ingestao')
-def chamar_ingestao():
+def chamar_ingestao(csv_file):
     try:
-        csv_file = extrair_dados()
         if csv_file:
             Ingestao.processar_dados(csv_file)
             print("Ingestão concluída com sucesso.")
-            return jsonify({"message": "Ingestão concluída com sucesso."})
         else:
             print("Ingestão não realizada devido a erro na extração.")
-            return jsonify({"message": "Ingestão não realizada devido a erro na extração."}), 500
     except Exception as e:
         print(f"Erro na ingestão: {e}")
-        return jsonify({"message": f"Erro na ingestão: {e}"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    csv_file = chamar_extracao()
+    chamar_ingestao(csv_file)
